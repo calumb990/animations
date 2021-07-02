dd46637b49a1e60757300dbc224abf30d79661a6
Dyrektrypt <csbeale@btinternet.com>
2021-07-02 18:44:30 +0100

    Updated animations support for UMD, however needing to be transpiled

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    animations: './index.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: 'babel-loader'
+      },
+    ]
+  },
+  resolve: { 
+    extensions: ['.js', '.ts'] 
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: '[name].js',
+    library:  {
+      name: 'animations',
+      type: 'umd'
+    },
+    globalObject: 'this'
+  },
+}
